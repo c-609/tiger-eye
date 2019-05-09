e7c19c7322a9f6be08eb50d0ad705a0a62ad8c74
moon927 <411840982@qq.com>
2019-05-09 16:07:26 +0800

    tj

---
+++ src/components/pages/index/person/InfoManage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="userName">
+        <el-input :disabled="true" v-model="ruleForm.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号" prop="phoneNumber">
+        <el-input v-model.number="ruleForm.phoneNumber"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'InfoManage',
+    data() {
+      var checkPhoneNumber = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('手机号不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      return {
+        ruleForm: {
+          userName: '',
+          phoneNumber: '',
+        },
+        rules: {
+          phoneNumber: [
+            { validator: checkPhoneNumber, trigger: 'blur' }
+          ],
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
